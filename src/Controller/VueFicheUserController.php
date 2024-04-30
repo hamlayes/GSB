c34aa006e42f6e6e2a30032ee9848b3dc3d3ab6a
hamlaoui976 <hamlaoui976@gmail.com>
2024-04-30 14:52:01 +0200

    J'AI FINIIIIIIIIII

--- src/Controller/VueFicheUserController.php
+++ src/Controller/VueFicheUserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
 use App\Entity\FicheFrais;
 use App\Entity\FraisForfait;
 use App\Form\FicheFraisType;
+use App\Repository\EtatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -15,19 +17,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class VueFicheUserController extends AbstractController
 {
     #[Route('/vue/fiche/user/{id}', name: 'app_vue_fiche_user')]
-    public function index($id, EntityManagerInterface $entityManager): Response
+    public function index($id, EntityManagerInterface $entityManager, Request $request, EtatRepository $etatRepository): Response
     {
         $ficheFrais = $entityManager->getRepository(FicheFrais::class)->find($id);
         $fraisForfait = $entityManager->getRepository(FraisForfait::class)->findAll();
+        $etat = $entityManager->getRepository(Etat::class)->findAll();
 
         if (!$ficheFrais) {
             throw $this->createNotFoundException('No fiche found for id '.$id);
         }
 
+        $form = $this->createForm(\App\Form\ChangeEtatType::class, null, ['allEtat' => $etatRepository->findAll()]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $etat = $form->get('Etat')->getData();
+            $ficheFrais->setEtat($etat);
+            $entityManager->persist($ficheFrais);
+            $entityManager->flush();
+
+        }
+
+
+
         return $this->render('vue_fiche_user/index.html.twig', [
             'controller_name' => 'VueFicheUserController',
             'ficheFrais' => $ficheFrais,
             'fraisForfait'=>$fraisForfait,
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/ChangeEtatType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangeEtatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Etat', ChoiceType::class, [
+                'choices' => $options['allEtat'],
+                'choice_label' => function (Etat $etat) {
+                    return $etat->getLibelle();
+                },
+                'attr'=>[
+                    'class'=>'form-control dropdown-toggle'
+                ]
+
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'valider',  // Texte du bouton
+                'attr' => [
+                    'class' => 'btn btn-success',  // Classes CSS du bouton
+                    'style' => 'font-size: 16px;', // Styles CSS supplémentaires
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+        $resolver->setDefined(['allEtat']); // Add this line
+    }
+}
